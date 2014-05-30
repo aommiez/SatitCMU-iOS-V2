782e0dae56601e6dfeb861d116fdd629f8c1c49b
aommiez <aommiez@gmail.com>
2014-05-30 11:46:15 +0700

    update

---
+++ SatitCMU/View/PFIndexViewController.m
@@ -0,0 +1,38 @@
+//
+//  PFIndexViewController.m
+//  SatitCMU
+//
+//  Created by MRG on 5/30/2557 BE.
+//  Copyright (c) 2557 Platwo fusion. All rights reserved.
+//
+
+#import "PFIndexViewController.h"
+
+@interface PFIndexViewController ()
+
+@end
+
+@implementation PFIndexViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
